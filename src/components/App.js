2ee00a8dd73651640abdb93fcd7dfad4463fa59d
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-05-16 19:53:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React,{useState} from "react";
 import "./../styles/App.css";
 
-function App() 
-{
+function App() {
+const [lists,setLists]=useState();
+const [item,setItem]=useState([]);
+const [edit,setEdit]=useState();
+const [update,setUpdate]=useState();
+const click=(e)=>{
+	e.preventDefault();
+	setItem=((oldval)=>{
+      return [...oldval,lists]
+	})
+   setLists("");
+}
+const edits=(id)=>{
+	const edited=lists.find((ele,ind)=>{
+		return(<>
+		ind===id
+		</>) 
+	});
+	setEdit(edited);
+}
+const updates=()=>{
+	return(
+		<>
+		<input className="editTask" type="text" value={edit} onChange={(e)=>setUpdate(e.target.value)}/>
+		<button className="saveTask" onClick={()=>setItem(update)}>Save</button>
+		</>
+	)
+}
+
+const del=(id)=>{
+const uplists=lists.filter((ele,ind)=> id!==ind)
+   setLists([...uplists]);
+}
+
 	return (
 	<div id="main">
 	//Do not alter main div
 	//Please do not alter the functional component as tests depend on the type of component.
+	<input id="task" type="text" placeholder="Add Task" onChange={(e)=>setLists(e.target.value)} value={lists} />
+	<Button id="btn" onClick={click}>ADD Task</Button>
+    {
+		item.map((cur,ind)=>{
+			return(
+				<ul key={ind}>
+					<li className="list">{cur}</li>
+					<button className="edit" onClick={()=>edits(ind)}>edit</button>
+					<button className="delete" onClick={()=>del(ind)}>delete</button>
+				</ul>
+			)
+		})
+	}
+
 	</div>
 	);
 }
